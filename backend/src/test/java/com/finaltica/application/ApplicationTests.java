9e3ee2bfee06bb4db75b2724196679621097c2e2
Adharsh Rengarajan <adharshr0613@gmail.com>
2026-01-14 16:54:16 -0500

    Remove SpringBootTest annotation from ApplicationTests

--- backend/src/test/java/com/finaltica/application/ApplicationTests.java
+++ backend/src/test/java/com/finaltica/application/ApplicationTests.java
@@ -3,7 +3,6 @@ package com.finaltica.application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
 
 	@Test
